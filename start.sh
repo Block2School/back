8804b7ac7bc6e62383f81410d6f2c594dd96d3ba
Cyril Grosjean <cyril.grosjean@epitech.eu>
2022-09-03 11:40:53 -0400

    fix : Updated host

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ pip install -r requirements.txt
 echo 'Checking database...'
 bash scripts/update_db.sh
 echo 'Starting server...'
-cd src && uvicorn main:app --reload --port=8080
+cd src && uvicorn main:app --reload --host=0.0.0.0 --port=8080
