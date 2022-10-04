d52032cf4304aeaa849423c147fa801d94896b96
Lorenzo Manoeuvre <lorenzo.manoeuvre@epitech.eu>
2022-10-05 00:33:55 +0200

    modif UserTutorialScore.update to check lines characters or both & modif tutorial.py check create or update tutoScore

--- src/routes/tutorial.py
+++ src/routes/tutorial.py
@@ -142,8 +142,20 @@ async def complete_tutorial(tutorial: SubmitTutorialModel, credentials: str = De
             if (tuto != None and tuto['answer'] == r['output']):
                 result = TutorialService.validate_tutorial(jwt['uuid'], tutorial.tutorial_id)
                 print("result => ", result)
-                newScore = userTutorialScoreDb.insert(jwt['uuid'], tutorial.tutorial_id, tutorial.language, tutorial.characters, tutorial.lines)
-                print(newScore)
+                # FETCH si None insert otherwise update
+                fetch = userTutorialScoreDb.fetch(jwt['uuid'], tutorial.tutorial_id, tutorial.language)
+                if (fetch == None):
+                    newScore = userTutorialScoreDb.insert(jwt['uuid'], tutorial.tutorial_id, tutorial.language, tutorial.characters, tutorial.lines)
+                else:
+                    if (tutorial.characters < fetch['characters'] and tutorial.lines < fetch['lines']):
+                        print("both")
+                        updateScore = userTutorialScoreDb.update(jwt['uuid'], tutorial.tutorial_id, tutorial.language, tutorial.characters, tutorial.lines)
+                    elif (tutorial.characters < fetch['characters'] and tutorial.lines >= fetch['lines']):
+                        print('characters')
+                        updateScore = userTutorialScoreDb.update(jwt['uuid'], tutorial.tutorial_id, tutorial.language, tutorial.characters, -1)
+                    elif (tutorial.characters >= fetch['characters'] and tutorial.lines < fetch['lines']):
+                        print('lines')
+                        updateScore = userTutorialScoreDb.update(jwt['uuid'], tutorial.tutorial_id, tutorial.language, -1, tutorial.lines)
                 return JSONResponse({'is_correct': True, 'total_completions': result, 'error_description': None})
             else:
                 return JSONResponse({'is_correct': False, 'total_completions': 0, 'error_description': None})
