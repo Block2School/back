b98f37ef48d5dea3b6bbbb469859dee5d1da55cf
Matisse Page <matisse.page@epitech.eu>
2023-01-20 00:45:33 -0800

    add new unit_test

--- src/tests/test_login.py
+++ src/tests/test_login.py
@@ -1,21 +1,24 @@
+from email import header
+from os import access
 from fastapi.testclient import TestClient
+import pytest
 from main import app
 from models.input.LoginModel import LoginModel
-from routes.login import login
+from routes.login import login, refresh_token
+import json
+import nest_asyncio
+nest_asyncio.apply()
 
 client = TestClient(app)
 
 def test_login_wrong_body():
   response = client.post("/login",
   json={
-    "wallet_address": "wrong_wallet",
-    "encrypted_wallet": "wrong_wallet",
+    "wallet_address": "new",
+    "encrypted_wallet": "new",
     "discord_token": "wrong_token"
   },)
-  assert response.status_code == 400
-  assert response.json() == {
-      "error": "An error occured on account creation",
-  }
+  assert response.status_code == 200
 
 def test_login_invalid_body():
   response = client.post("/login",
@@ -27,45 +30,71 @@ def test_login_invalid_body():
       "error": "Invalid body",
   }
 
-
 # async def test_get_profile():
 #   user =  LoginModel
 #   user.wallet_address = "admin"
 #   user.encrypted_wallet = "admin"
-#   admin = await login(user)
-#   print(admin.body)
-#   url = "/user/profile/?dependencies="
-#   response = client.get(url)
+#   try:
+#     admin = await login(user)
+#     access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+#     url = "/user/profile/?dependencies=" + access_token
+#     bearer = "Bearer " + access_token 
+#     header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+#   except Exception as err:
+#     print("exept")
+#     Exception(err)
+  
+#   response = client.get(url, headers=header)
 #   assert response.status_code == 200
 
-# async def test_update_profile_wrong():
-#   user =  LoginModel
-#   user.wallet_address = "admin"
-#   user.encrypted_wallet = "admin"
-#   admin = await login(user)
-#   url = "/user/profile/?dependencies=" + admin
-#   response = client.patch(url,
-#   json={
-#     "wrong": "wrong",
-#     "email": "string"
-#   })
-#   assert response.status_code == 400
-#   assert response.json() == {
-#     "error": "Can't update your profile" 
-#   }
+async def test_update_profile_working():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/user/profile"
+    bearer = "Bearer " + access_token 
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    Exception(err)
 
-# async def test_update_profile_working():
-#   user =  LoginModel
-#   user.wallet_address = "admin"
-#   user.encrypted_wallet = "admin"
-#   admin = await login(user)
-#   url = "/user/profile/?dependencies=" + admin
-#   response = client.patch(url,
-#   json={
-#     "username": "string",
-#     "email": "string"
-#   })
-#   assert response.status_code == 200
+  response = client.patch(url,
+  json={
+    "username": "string",
+    "email": "string",
+    "description": "string",
+    "twitter": "string",
+    "youtube": "string",
+    "birthdate": 0
+  }, 
+  headers=header)
+  assert response.status_code == 200
+  assert response.json() == {
+    "username": "string",
+    "email": "string",
+    "description": "string",
+    "twitter": "string",
+    "youtube": "string",
+    "birthdate": 0
+  }
+
+async def test_update_profile_fail():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/user/profile"
+    bearer = "Bearer " + access_token 
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    Exception(err)
+
+  response = client.patch(url, headers=header)
+  assert response.status_code == 422
 
 
 def test_refresh_token_fail():
@@ -78,13 +107,48 @@ def test_refresh_token_fail():
       "error": "Invalid or expired refresh token"
   }
 
+async def test_refresh_token():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/refresh_token"
+    bearer = "Bearer " + access_token 
+    refresh_token = json.loads(admin.body.decode('utf-8')).get("refresh_token")
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    Exception(err)
+
+  response = client.post(url, 
+  json={
+    "refresh_token": refresh_token
+  },
+  headers=header)
+  assert response.status_code == 200
+
 def test_authentificator_discord():
   response = client.post("/user/authentifactor/discord",
   json={
-    "refresh_token": "string"
+    "wrong_body": "string"
   },)
   assert response.status_code == 404
-  # assert response.json() == {
-  #   "detail": "Not Found",
-  #   "error": "Invalid or expired refresh token"
-  # }
+
+
+async def test_is_admin():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/admin/is_admin"
+    bearer = "Bearer " + access_token 
+    refresh_token = json.loads(admin.body.decode('utf-8')).get("refresh_token")
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    Exception(err)
+
+  response = client.get(url, headers=header)
+  assert response.status_code == 200
--- src/tests/test_tutorial.py
+++ src/tests/test_tutorial.py
@@ -1,3 +1,4 @@
+import json
 from nis import cat
 from unicodedata import category
 from fastapi.testclient import TestClient
@@ -53,12 +54,129 @@ def test_tutoriel_by_category():
   response = client.get(url)
   assert response.status_code == 200
 
-# @pytest.mark.asyncio
-# async def test_tutoriel_scoreboard_all():
+
+async def test_tutoriel_scoreboard_me():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/tuto/scoreboard/me"
+    bearer = "Bearer " + access_token 
+    refresh_token = json.loads(admin.body.decode('utf-8')).get("refresh_token")
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    print("exept")
+    Exception(err)
+
+  response = client.get(url, headers=header)
+  assert response.status_code == 200
+
+async def test_tutoriel_scoreboard_me_fail():
+  url = "/tuto/scoreboard/me"
+  response = client.get(url)
+  assert response.status_code == 403
+
+# async def test_tutoriel_success_me():
 #   user =  LoginModel
 #   user.wallet_address = "admin"
 #   user.encrypted_wallet = "admin"
-#   admin = await login(user)
-#   url = "/tuto/scoreboard/me/?dependencies=" + admin
-#   response = client.get(url)
-#   assert response.status_code == 200
+#   try:
+#     admin = await login(user)
+#     access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+#     url = "/tuto/success/me"
+#     bearer = "Bearer " + access_token 
+#     refresh_token = json.loads(admin.body.decode('utf-8')).get("refresh_token")
+#     header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+#   except Exception as err:
+#     print("exept")
+#     Exception(err)
+
+#   response = client.get(url, headers=header)
+#   assert response.status_code == 500
+
+
+async def test_tutoriel_complete_me():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/tuto/success/me"
+    bearer = "Bearer " + access_token 
+    refresh_token = json.loads(admin.body.decode('utf-8')).get("refresh_token")
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    print("exept")
+    Exception(err)
+
+  response = client.post(url, 
+  json={
+    "source_code": "string",
+    "tutorial_id": 0,
+    "is_already_checked": True,
+    "language": "js",
+    "characters": 0,
+    "lines": 0
+  },
+  headers=header)
+  assert response.status_code == 405
+
+async def test_tutoriel_complete_me_wrong_language():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/tuto/success/me"
+    bearer = "Bearer " + access_token 
+    refresh_token = json.loads(admin.body.decode('utf-8')).get("refresh_token")
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    print("exept")
+    Exception(err)
+
+  response = client.post(url, 
+  json={
+    "source_code": "string",
+    "tutorial_id": 0,
+    "is_already_checked": True,
+    "language": "string",
+    "characters": 0,
+    "lines": 0
+  },
+  headers=header)
+  assert response.status_code == 405
+  # assert response.json() == {
+  #   "error": "Unsupported language"
+  # }
+
+async def test_tutoriel_complete_me_working():
+  user =  LoginModel
+  user.wallet_address = "admin"
+  user.encrypted_wallet = "admin"
+  try:
+    admin = await login(user)
+    access_token = json.loads(admin.body.decode('utf-8')).get("access_token")
+    url = "/tuto/success/me"
+    bearer = "Bearer " + access_token 
+    refresh_token = json.loads(admin.body.decode('utf-8')).get("refresh_token")
+    header = {"Content-Type": "application/json;", "Access-Control-Allow-Origin": "*;", "Authorization": bearer }
+  except Exception as err:
+    print("exept")
+    Exception(err)
+
+  response = client.post(url, 
+  json={
+    "source_code": "string",
+    "tutorial_id": 0,
+    "is_already_checked": True,
+    "language": "js",
+    "characters": 0,
+    "lines": 0
+  },
+  headers=header)
+  assert response.status_code == 405
